66a745a73d8df95e3830f7a6337efca1c080d0cf
Gabe C <113395303+GabeVC@users.noreply.github.com>
2023-11-05 06:05:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PatientPage from './PatientPage';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import NursePage from './NursePage';
 
 function App() {
   return (
-    <div className="App">
-      <PatientPage />
-      {/* Add more components/content here */}
-    </div>
+    <Router>
+      <Routes>
+      <Route exact path="/" element={<PatientPage/>} />
+      <Route path="/NursePage" element={<NursePage/>} />
+      </Routes>
+    </Router>
   );
 }
 
